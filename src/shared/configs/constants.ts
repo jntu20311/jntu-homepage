abc0d28db19352abcfcd37c47003dac981e52337
dev-hoony <dev_hoony@naver.com>
2026-09-05 17:19:17 +0900

    fix : footer 디자인 수정 - 메뉴 제거

--- src/shared/configs/constants.ts
+++ src/shared/configs/constants.ts
@@ -13,9 +13,9 @@ export const CONSTANTS = {
 
   // 홈페이지 정보
   REPRESENTATIVE: "대표자 : 김신안",
-  NUMBER: "고유번호: 231-82-69370",
+  NUMBER: "고유번호 : 231-82-69370",
   ADDRESS: "주소 : 전라남도 무안군 일로읍 오남로1길 9-4 제이타워 5층",
   POSTCODE: "58581",
-  TEL: "전화: 010-6663-0311",
-  COPYRIGHT: "©전남광주교사노동조합 All right reserved.",
+  TEL: "전화 : 010-6663-0311",
+  COPYRIGHT: "©2020 전남광주교사노동조합 All right reserved.",
 };
--- src/widgets/footer/ui/footer.tsx
+++ src/widgets/footer/ui/footer.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { legalItems, navMenus } from "@/shared/configs/nav";
+import { legalItems } from "@/shared/configs/nav";
 import { routes } from "@/shared/configs/routes";
 import { Logo } from "@/shared/ui/logo";
 import { CONSTANTS } from "@/shared/configs/constants";
@@ -8,23 +8,26 @@ export const Footer = () => {
   return (
     <footer className="border-t border-border bg-background">
       <div className="mx-auto w-full max-w-6xl px-4 py-10 sm:px-6 lg:px-8">
-        <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-4">
+        {/* <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-4"> */}
+        <div className="gap-8">
           {/* Brand */}
           <div className="space-y-3 sm:col-span-2 lg:col-span-1">
             <Link to={routes.ROOT} className="text-lg font-bold tracking-tight">
               <Logo className="w-65" />
             </Link>
 
-            <address className="space-y-1 text-sm not-italic text-muted-foreground">
-              <p>{CONSTANTS.REPRESENTATIVE}</p>
-              <p>{CONSTANTS.NUMBER}</p>
+            <address className="w-full text-sm not-italic text-muted-foreground mt-4 flex flex-col gap-y-1">
+              <div className="flex items-center gap-x-4">
+                <p>{CONSTANTS.REPRESENTATIVE}</p>
+                <p>{CONSTANTS.NUMBER}</p>
+              </div>
               <p>{CONSTANTS.ADDRESS}</p>
               <p>{CONSTANTS.TEL}</p>
             </address>
           </div>
 
           {/* Menu columns */}
-          {navMenus
+          {/* {navMenus
             .filter((menu) => menu.items)
             .map((menu) => (
               <div key={menu.label} className="space-y-3">
@@ -42,7 +45,7 @@ export const Footer = () => {
                   ))}
                 </ul>
               </div>
-            ))}
+            ))} */}
         </div>
 
         <div className="mt-8 flex flex-col items-center justify-between gap-4 border-t border-border pt-6 text-sm text-muted-foreground sm:flex-row">
@@ -52,7 +55,7 @@ export const Footer = () => {
               <Link
                 key={item.path}
                 to={item.path}
-                className="transition-colors hover:text-foreground"
+                className={`transition-colors hover:text-foreground ${item.path == routes.PRIVACY && "font-semibold"}`}
               >
                 {item.label}
               </Link>
