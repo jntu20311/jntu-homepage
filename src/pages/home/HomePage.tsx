e3b47cae1a0e77aa56b7e038c794847f4cabc64d
dev-hoony <dev_hoony@naver.com>
2026-09-05 13:33:05 +0900

    fix :  기본 템플릿 App.tsx 변경 & navigate test

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,3 +1,19 @@
+import { routes } from "@/shared/configs/routes";
+import { useNavigate } from "react-router-dom";
+
 export const HomePage = () => {
-  return <>Home</>;
+  const navigate = useNavigate();
+
+  return (
+    <>
+      Home
+      <button
+        onClick={() => {
+          navigate(routes.ABOUT);
+        }}
+      >
+        button
+      </button>
+    </>
+  );
 };
