82fe4f0101e0d2389cdbfa5c52442658e44a2320
dev-hoony <dev_hoony@naver.com>
2026-09-05 21:51:38 +0900

    fix : 조합원 가입, 후원회원 가입 - 이미지 링크 추가

--- src/pages/join/supporter/JoinSupporterPage.tsx
+++ src/pages/join/supporter/JoinSupporterPage.tsx
@@ -1,5 +1,24 @@
-import { PageHeader } from "@/shared/ui/page-header";
+import { Link } from "react-router-dom";
 
 export const JoinSupporterPage = () => {
-  return <PageHeader title="후원회원 가입" description="후원회원으로 가입하세요." />;
+  return (
+    <div>
+      <header className="flex items-center gap-2 mb-4">
+        <h1 className="text-2xl font-bold tracking-tight sm:text-3xl">
+          후원회원 가입
+        </h1>
+      </header>
+      <Link to={"https://www.naver.com/"} target="_blank">
+        <div className="flex w-full items-center justify-center text-white text-3xl font-bold bg-green-600 h-200">
+          {"후원회원가입 관련 이미지 \n(클릭시 네이버로 이동)"}
+        </div>
+      </Link>
+
+      <Link to={"https://www.google.com/"} target="_blank">
+        <div className="flex w-full items-center justify-center text-white text-3xl font-bold bg-blue-600 h-200">
+          {"후원회원가입 관련 이미지2 \n(클릭시 구글로 이동)"}
+        </div>
+      </Link>
+    </div>
+  );
 };
